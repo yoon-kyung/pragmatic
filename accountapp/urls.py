8d775ffd2a07812e5530a75cead456bb5f7e7c75
yoonkyungOh <iamoyk58@gmail.com>
2021-12-25 05:54:27 +0900

    hello world 완료

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -3,5 +3,5 @@ from accountapp.views import hello_world
 
 app_name = "accountapp"
 urlpatterns=[
-    path('hello_world', hello_world, name='hello world')
+    path('hello_world', hello_world, name='hello_world')
 ]
